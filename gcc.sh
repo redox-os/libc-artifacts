8d6a16bbca95f3308a77378e018aa852d179dd6e
Jeremy Soller <jackpot51@gmail.com>
2017-01-04 10:56:25 -0700

    Update for OS X

--- gcc.sh
+++ gcc.sh
@@ -12,6 +12,18 @@ then
 	"$ROOT/usr/lib/libm.a" \
 	"$ROOT/usr/lib/libgcc.a" \
 	"$ROOT/usr/lib/libc.a"
+elif [ "`uname`" == "Darwin" ]
+then
+	C_INCLUDE_PATH="$ROOT/usr/include" \
+	LIBRARY_PATH="$ROOT/usr/lib" \
+	x86_64-elf-gcc -v \
+	-static -nostartfiles -nostdlib -nodefaultlibs \
+	-undef -imacros "$ROOT/define.h" \
+	"$@" \
+	"$ROOT/usr/lib/crt0.o" \
+	"$ROOT/usr/lib/libm.a" \
+	"$ROOT/usr/lib/libgcc.a" \
+	"$ROOT/usr/lib/libc.a"
 else
 	echo "$0: `uname` not supported"
 	exit 1
