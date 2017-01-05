67098b38c671d7804c3b9b7b856090099d4cecda
Jeremy Soller <jackpot51@gmail.com>
2017-01-05 13:28:42 -0700

    Cleanup OS X vs Linux

--- gcc.sh
+++ gcc.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
 ROOT="$(cd `dirname $0` && pwd)"
+
 if [ "`uname`" == "Linux" ]
 then
-	C_INCLUDE_PATH="$ROOT/usr/include" \
-	LIBRARY_PATH="$ROOT/usr/lib" \
-	gcc -v \
-	-static -nostartfiles -nostdlib -nodefaultlibs \
-	--sysroot "$ROOT" \
-	-undef -imacros "$ROOT/define.h" \
-	"$@" \
-	"$ROOT/usr/lib/crt0.o" \
-	"$ROOT/usr/lib/libm.a" \
-	"$ROOT/usr/lib/libgcc.a" \
-	"$ROOT/usr/lib/libc.a"
+	compiler="gcc"
 elif [ "`uname`" == "Darwin" ]
 then
-	C_INCLUDE_PATH="$ROOT/usr/include" \
-	LIBRARY_PATH="$ROOT/usr/lib" \
-	x86_64-elf-gcc -v \
-	-static -nostartfiles -nostdlib -nodefaultlibs \
-	--sysroot "$ROOT" \
-	-undef -imacros "$ROOT/define.h" \
-	"$@" \
-	"$ROOT/usr/lib/crt0.o" \
-	"$ROOT/usr/lib/libm.a" \
-	"$ROOT/usr/lib/libgcc.a" \
-	"$ROOT/usr/lib/libc.a"
+	compiler="x86_64-elf-gcc"
 else
 	echo "$0: `uname` not supported"
 	exit 1
 fi
+
+C_INCLUDE_PATH="$ROOT/usr/include" \
+LIBRARY_PATH="$ROOT/usr/lib" \
+"$compiler" \
+-static -nostartfiles -nostdlib -nodefaultlibs \
+--sysroot "$ROOT" \
+-undef -imacros "$ROOT/define.h" \
+"$@" \
+"$ROOT/usr/lib/crt0.o" \
+"$ROOT/usr/lib/libm.a" \
+"$ROOT/usr/lib/libgcc.a" \
+"$ROOT/usr/lib/libc.a"
